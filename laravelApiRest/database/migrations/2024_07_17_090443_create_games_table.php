27014224f61d826975567ff3d747e39bcfc0eb8b
Rossana Liendo <rossana.liendo@gmail.com>
2024-07-17 11:11:31 +0200

    Completed migrations for my tables User and Game

---
+++ laravelApiRest/database/migrations/2024_07_17_090443_create_games_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->integer('dice1');
+            $table->integer('dice2');
+            $table->integer('result');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
