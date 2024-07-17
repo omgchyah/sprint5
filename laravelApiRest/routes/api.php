2de2379fca3d16126e13644cdfe92a4042c72786
Rossana Liendo <rossana.liendo@gmail.com>
2024-07-17 13:49:40 +0200

    Starting the routes

--- laravelApiRest/routes/api.php
+++ laravelApiRest/routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::group(['namespace' => 'App\Http\Controllers'], function(){
+    Route::get('/users', [UserController::class, 'index']);
+
+});
