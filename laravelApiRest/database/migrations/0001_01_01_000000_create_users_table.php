afa01ff0ab96c49c5d407d7ee7687b046eb57cc3
Rossana Liendo <rossana.liendo@gmail.com>
2024-07-17 10:51:41 +0200

    Edited tables for user

--- laravelApiRest/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravelApiRest/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nickname')->unique();
+            $table->string('name')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->enum('role', ['admin', 'user', 'guest']);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
