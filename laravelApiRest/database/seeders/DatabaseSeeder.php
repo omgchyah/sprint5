85f2abcde92d14a40c8671184586531d1df926a6
Rossana Liendo <rossana.liendo@gmail.com>
2024-07-23 13:51:48 +0200

    Migrate from fresh

--- laravelApiRest/database/seeders/DatabaseSeeder.php
+++ laravelApiRest/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,10 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+        // Create the admin user
+        User::create([
+                    'nickname' => 'admin',
+                    'name' => 'Admin User',
+                    'email' => 'admin@example.com',
+                    'email_verified_at' => now(),
+                    'role' => 'admin',
+                    'password' => Hash::make('password'),
+                    'remember_token' => Str::random(10),
+                ]);
         // User::factory(10)->create();
 
         User::factory()
-            ->count(10)
+            ->count(20)
             ->hasGames(10)
             ->create();
     }
