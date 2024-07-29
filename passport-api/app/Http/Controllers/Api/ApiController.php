5ca88847dea3d0ff8422a44f7ab8342453d0bec8
Rossana Liendo <rossana.liendo@gmail.com>
2024-07-29 15:04:16 +0200

    apicontroller created

---
+++ passport-api/app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    // POST [name, email, password]
+    public function register(Request $request)
+    {
+    
+    }
+    
+    // POST [ email, password]
+    public function login(Request $request)
+    {
+        
+    }
+}
