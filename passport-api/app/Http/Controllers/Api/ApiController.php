6ce723bffbbaa6c44d2be2ec05eb47f0ee887bd8
Rossana Liendo <rossana.liendo@gmail.com>
2024-07-31 11:12:57 +0200

    register method complete

--- passport-api/app/Http/Controllers/Api/ApiController.php
+++ passport-api/app/Http/Controllers/Api/ApiController.php
@@ -4,13 +4,31 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use app\Models\User;
 
 class ApiController extends Controller
 {
     // POST [name, email, password]
     public function register(Request $request)
     {
-    
+        //Validation
+        $request->validate([
+            "name" => "required|string",
+            "email" => "required|string|email|unique:users",
+            "password" => "required"
+        ]);
+
+        //Create User
+        User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => bcrypt($request->password)
+        ]);
+        return response()->json([
+            "status" => true,
+            "message" => "User registered successfully",
+            "data" => []
+        ]);
     }
     
     // POST [ email, password]
