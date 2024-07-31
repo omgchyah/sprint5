b21491435a68b610020c0562a4113de8ef71875a
Rossana Liendo <rossana.liendo@gmail.com>
2024-07-31 10:59:17 +0200

    wrote protected routes

--- passport-api/routes/api.php
+++ passport-api/routes/api.php
@@ -9,6 +9,12 @@ Route::post("register", [ApiController::class, "register"]);
 Route::post("login", [ApiController::class, "login"]);
 
 //Protected Routes
+Route::group([
+    "middleware" => ["auth:api"]
+], function(){
+    Route::get("profile", [ApiController::class, "profile"]);
+    Route::get("logout", [ApiController::class, "logout"]);
+});
 
 /* Route::get('/user', function (Request $request) {
     return $request->user();
